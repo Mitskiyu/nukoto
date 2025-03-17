4c554410f8957d6b12f70ab0b815e14036f6f34e
Denzel <denzel.mitski@gmail.com>
2025-03-17 04:59:41 +0100

    feat: add settings state management

--- client/src/components/settings.tsx
+++ client/src/components/settings.tsx
@@ -1,21 +1,27 @@
+import { useImageStore, Format } from "../lib/image-store";
 import arrow from "../assets/images/arrow.svg";
 
 function Settings() {
+	const updateSettings = useImageStore((state) => state.updateSettings);
+	const currentQuality = useImageStore((state) => state.settings.quality);
+
 	const handleQuality = (e: React.ChangeEvent<HTMLInputElement>) => {
-		//TODO
+		const updatedQuality = +e.target.value;
+		updateSettings({ quality: updatedQuality });
 	};
 
 	const handleFormat = (e: React.ChangeEvent<HTMLSelectElement>) => {
-		//TODO
+		const updatedFormat = e.target.value as Format;
+		updateSettings({ format: updatedFormat });
 	};
 
 	return (
-		<div className="w-full font-koden text-text h-[360px] flex flex-col bg-surface0 outline-1 outline-surface1 rounded-sm">
+		<div className="w-full font-koden text-text sm:h-[360px] h-[280px] flex flex-col bg-surface0 outline-1 outline-surface1 rounded-sm">
 			<h1 className="text-2xl mb-4 ml-3.5 mt-3 text-lavender">Settings</h1>
 			<div className="w-full flex flex-col -mt-2">
 				<div className="flex justify-between items-center w-11/12 mx-auto">
 					<label className="text-xl">Quality</label>
-					<span className="text-lg text-mauve">80</span>
+					<span className="text-lg text-mauve">{currentQuality}</span>
 				</div>
 				<div className="w-11/12 mx-auto">
 					<input
@@ -23,7 +29,7 @@ function Settings() {
 						min="1"
 						max="100"
 						step="1"
-						defaultValue="80"
+						value={currentQuality}
 						onChange={handleQuality}
 						className="w-full h-2 focus:outline-none bg-surface1 rounded-full appearance-none [&::-webkit-slider-thumb]:appearance-none [&::-webkit-slider-thumb]:h-3 [&::-webkit-slider-thumb]:w-3 [&::-webkit-slider-thumb]:rounded-full [&::-webkit-slider-thumb]:bg-mauve"
 					/>
