588063544030d8d7d50fb91a91f32e4b000364c0
Denzel <denzel.mitski@gmail.com>
2025-03-25 08:24:52 +0100

    feat: add toast component

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,9 +2,23 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
+import { Toaster } from "sonner";
+import nukoEmbarrassed from "./assets/images/nukoEmbarrassed.gif";
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
+		<Toaster
+			theme="dark"
+			richColors
+			closeButton
+			style={{
+				fontFamily: "Kodenmachou, sans-serif",
+			}}
+			position={window.innerWidth < 640 ? "top-center" : "top-right"}
+			icons={{
+				error: <img src={nukoEmbarrassed} alt="nuko cat embarrassed" />,
+			}}
+		/>
 		<App />
 	</StrictMode>,
 );
