e5d36865e4e3f8489e7481cbe71ed6f48545ec32
Denzel <denzel.mitski@gmail.com>
2025-03-29 09:18:33 +0100

    feat: add app layout

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,25 @@
+import Header from "./components/header";
+import Upload from "./components/upload";
+import Settings from "./components/settings";
+import Output from "./components/output";
+import Footer from "./components/footer";
+
 function App() {
-	return <div className="text-3xl">nukoto</div>;
+    return (
+        <div className="min-h-screen flex flex-col">
+            <Header />
+            <main className="container flex-1 w-full max-w-6xl mx-auto px-4 sm:mt-20 mt-4 grid md:grid-cols-12 gap-4">
+                <div className="md:col-span-4 col-span-1 space-y-4">
+                    <Upload />
+                    <Settings />
+                </div>
+                <div className="md:col-span-8 col-span-1">
+                    <Output />
+                </div>
+            </main>
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
