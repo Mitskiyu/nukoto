078d5ef48f49bb4e9b27e5fe8f99bd52e6471535
Denzel <denzel.mitski@gmail.com>
2025-03-30 11:54:33 +0200

    style: footer mt

--- client/src/components/footer.tsx
+++ client/src/components/footer.tsx
@@ -1,14 +1,12 @@
 function Footer() {
     return (
-        <footer className="w-full bg-mantle border-t-crust border-t-2">
-            <div className="w-full max-w-6xl px-4 mx-auto py-4 flex items-center justify-center">
+        <footer className="w-full bg-mantle border-t-crust border-t-2 h-8 sm:h-12 sm:mt-0 mt-4">
+            <div className="w-full max-w-6xl mx-auto sm:py-2 flex items-center justify-center">
                 <p className="font-koden text-lavender text-lg">
                     nukoto <span className="text-mauve">•</span> {new Date().getFullYear()}
                 </p>
             </div>
         </footer>
-
     )
 }
-
 export default Footer;
